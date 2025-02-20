e717c29778047b3c0bcc33ea705583f9e0d684f8
Evgeniy Rezanov <evgrezanov@gmail.com>
2025-02-20 15:35:27 +0700

    Ai fixes
    fix: adapt WooMS excerpt addon to new core plugin structure
    
    - Update API data handling for product excerpts
    - Fix custom field synchronization from MoySklad
    - Optimize settings integration
    - Add proper attribute filtering
    - Improve short description handling logic

--- wooms-excerpt.php
+++ wooms-excerpt.php
@@ -3,80 +3,159 @@
  * Plugin Name: WooMS Excerpt (extension)
  * Description: Краткое описание товара в МойСклад в дополнительном поле сохраняется в excerpt товара
  * Plugin URI: https://github.com/wpcraft-ru/wooms/issues/400
- * Version: 1.0
+ * Version: 1.1
  * Author: OGlekler
  * Author URI: https://github.com/OlaIola/
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * WooMS tested up to: 8.2.0
- * Version: 1.0
  *
  * @package WooMS Excerpt
  */
 
 namespace WooMS;
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-/**
- * Add excerpt from custom field in MoySklad
- */
 class ProductExcerpt {
-
 	/**
-	 * The Init
+	 * Initialization
+	 *
+	 * Hooks:
+	 * - plugins_loaded
+	 * - wooms_add_settings
+	 * - admin_init
+	 * - wooms_product_save
+	 * - wooms_attributes
 	 */
 	public static function init() {
-
-		add_action(
-			'plugins_loaded',
-			function () {
-
-				if ( ! class_exists( 'WooMS_Core' ) ) {
-
-					add_action( 'after_plugin_row_' . plugin_basename( __FILE__ ), array( __CLASS__, 'plugin_row' ), 5, 3 );
-
-					return;
-				}
-
-				/**
-				 * Depending on the version of Wooms the 'wooms_add_settings' hook can be absent
-				 */
-				add_action( 'wooms_add_settings', array( __CLASS__, 'add_settings' ), 20 );
-
-				/**
-				 * Action in case of absence of the 'wooms_add_settings' hook. It will be removed in the case where hook exist
-				 */
-				add_action( 'admin_init', array( __CLASS__, 'add_settings' ), 20 );
-
-				/**
-				 * If checked "Использовать краткое описание продуктов из дополнительного поля в МойСклад"
-				 */
-				if ( get_option( 'wooms_excerpt' ) ) {
-
-					add_filter( 'wooms_product_save', array( __CLASS__, 'update_custom_excerpt' ), 20, 2 );
-				}
-
-				/**
-				 * Preventing Wooms XT (Extra) from saving field with a short description like product attribute 
-				 * in case where "Включить синхронизацию доп. полей как атрибутов" (wooms_attr_enabled) is checked
-				 */
-				if ( get_option( 'wooms_excerpt_name' ) ) {
-
-					add_filter( 'wooms_attributes', array( __CLASS__, 'remove_short_description_from_product_attributes' ), 15, 3 );
-				}
+		add_action('plugins_loaded', function() {
+			if (!class_exists('WooMS_Core')) {
+				add_action('after_plugin_row_' . plugin_basename(__FILE__), array(__CLASS__, 'plugin_row'), 5, 3);
+				return;
 			}
-		);
+
+			add_action('wooms_add_settings', array(__CLASS__, 'add_settings'), 20);
+			add_action('admin_init', array(__CLASS__, 'add_settings'), 20);
+
+			if (get_option('wooms_excerpt')) {
+				add_filter('wooms_product_save', array(__CLASS__, 'update_custom_excerpt'), 20, 2);
+			}
+
+			// Предотвращаем сохранение поля краткого описания как атрибута
+			if (get_option('wooms_excerpt_name')) {
+				add_filter('wooms_attributes', array(__CLASS__, 'remove_short_description_from_product_attributes'), 15, 3);
+			}
+		});
 	}
 
 
 	/**
-	 * Wooms Excerpt: Wooms Check
+	 * Update product excerpt from MoySklad
 	 *
-	 * @param $plugin_file
-	 * @param $plugin_data
-	 * @param $status
+	 * @param WC_Product $product
+	 * @param array $data_api
+	 *
+	 * @return WC_Product
 	 */
+	public static function update_custom_excerpt($product, $data_api) {
+		$field_name = get_option('wooms_excerpt_name');
+		
+		if (!$field_name || empty($data_api['attributes'])) {
+			return $product;
+		}
+
+		$short_description = '';
+		
+		foreach ($data_api['attributes'] as $attribute) {
+			if (empty($attribute['name'])) {
+				continue;
+			}
+
+			if ($attribute['name'] == $field_name) {
+				$short_description = $attribute['value'];
+				
+				if (!get_option('wooms_short_description')) {
+					$product->set_short_description($short_description);
+				} else {
+					$product->set_description($short_description);
+				}
+				break;
+			}
+		}
+
+		// Очищаем краткое описание если оно отсутствует в МойСклад
+		if (empty($short_description)) {
+			if (!get_option('wooms_short_description')) {
+				$product->set_short_description('');
+			} else {
+				$product->set_description('');
+			}
+		}
+
+		return $product;
+	}
+
+	public static function add_settings() {
+		$option_name = 'wooms_excerpt';
+		
+		register_setting('mss-settings', $option_name);
+		register_setting('mss-settings', 'wooms_excerpt_name');
+
+		add_settings_field(
+			'wooms_excerpt',
+			'Использовать краткое описание из МойСклад',
+			function($args) {
+				printf(
+					'<input type="checkbox" name="%s" value="1" %s />',
+					$args['key'],
+					checked(1, $args['value'], false)
+				);
+				echo '<p><small>Активирует синхронизацию краткого описания из дополнительного поля МойСклад</small></p>';
+			},
+			'mss-settings',
+			'woomss_section_other',
+			[
+				'key' => $option_name,
+				'value' => get_option($option_name, 0)
+			]
+		);
+
+		add_settings_field(
+			'wooms_excerpt_name',
+			'Название поля для краткого описания',
+			function($args) {
+				printf(
+					'<input type="text" name="%s" value="%s" />',
+					$args['key'],
+					esc_attr($args['value'])
+				);
+				echo '<p><small>Укажите название дополнительного поля в МойСклад для краткого описания</small></p>';
+			},
+			'mss-settings',
+			'woomss_section_other',
+			[
+				'key' => 'wooms_excerpt_name',
+				'value' => get_option('wooms_excerpt_name', '')
+			]
+		);
+
+		remove_action('admin_init', array(__CLASS__, 'add_settings'), 20);
+	}
+
+	public static function remove_short_description_from_product_attributes($product_attributes, $product_id, $item) {
+		$field_name = get_option('wooms_excerpt_name');
+		
+		foreach ($product_attributes as $slug => $attribute) {
+			if ($attribute->get_name() == $field_name) {
+				unset($product_attributes[$slug]);
+				break;
+			}
+		}
+		
+		return $product_attributes;
+	}
+
 	public static function plugin_row( $plugin_file, $plugin_data, $status ) {
 
 		$base_name = plugin_basename( $plugin_file );
@@ -107,163 +186,6 @@ class ProductExcerpt {
 
 		<?php
 
-	}
-
-	/**
-	 * Add Settings (Options)
-	 */
-	public static function add_settings() {
-
-		self::add_setting_custom_excerpt();
-
-		remove_action( 'admin_init', array( __CLASS__, 'add_settings'), 20 );
-	}
-
-	/**
-	 * Settings (options) fields
-	 */
-	public static function add_setting_custom_excerpt()	{
-
-		/**
-		 * Checkmark to enable sync of custom excerpt
-		 */
-		$option_name = 'wooms_excerpt';
-
-		register_setting( 'mss-settings', $option_name );
-
-		add_settings_field(
-			$id       = $option_name,
-			$title    = 'Использовать краткое описание продуктов из дополнительного поля в МойСклад',
-			$callback = function ( $args ) {
-
-				printf(
-					'<input type="checkbox" name="%s" value="1" %s />',
-					$args['key'],
-					checked( 1, $args['value'], false )
-				);
-
-				printf(
-					'<p>%s</p>',
-					'Подробнее: <a href="https://github.com/wpcraft-ru/wooms/issues/400">https://github.com/wpcraft-ru/wooms/issues/400</a>'
-				);
-			},
-			$page     = 'mss-settings',
-			$section  = 'woomss_section_other',
-			$args     = array(
-				'key'   => $option_name,
-				'value' => get_option( $option_name, 0 ),
-			)
-		);
-
-		/**
-		 * Name of the field with custom excerpt
-		 */
-		$option_name = 'wooms_excerpt_name';
-
-		register_setting( 'mss-settings', $option_name );
-
-		add_settings_field(
-			$id	      = $option_name,
-			$title	  = 'Краткое описание товара',
-			$callback = function ( $args ) {
-				printf( '<input type="text" name="%s" value="%s" />', $args['key'], $args['value'] );
-				echo '<p><small>Укажите наименование поля с кратким описание товара в МойСклад. <br>Если дополнительное поле с таким названием будет заполнено у товара, оно будет сохраняться в Краткое описание товара на сайте.</small></p>';
-			},
-			$page     = 'mss-settings',
-			$section  = 'woomss_section_other',
-			$args     = array(
-				'key'   => $option_name,
-				'value' => sanitize_text_field( get_option( $option_name ) ),
-			)
-		);
-	}
-
-	/**
-	 * Update excerpt according to a custom field from MoySklad
-	 *
-	 * @param object $product — instance of a WC Product.
-	 * @param object $item — data from MoySklad.
-	 * @return object
-	 */
-	public static function update_custom_excerpt( $product, $item ) {
-
-		/**
-		 * Get name of the field with custom excerpt
-		 */
-		$field_name = get_option( 'wooms_excerpt_name' );
-
-		if ( $field_name ) {
-
-			$short_description_exist = 0;
-
-			if ( ! empty( $item['attributes'] ) ) {
-
-				foreach ( $item['attributes'] as $attribute ) {
-
-					if ( empty( $attribute['name'] ) ) {
-						continue;
-					}
-
-					if ( $attribute['name'] == $field_name ) {
-
-						$short_description_exist = 1;
-
-						/**
-						 * If checked "Использовать краткое описание продуктов вместо полного" - swap them
-						 * */
-						if ( ! get_option( 'wooms_short_description' ) ) {
-
-							$product->set_short_description( $attribute['value'] );
-						} else {
-
-							$product->set_description( $attribute['value'] );
-						}
-						break;
-					}
-				}
-			}
-
-			/**
-			 * If the short description isn't exist on MoySclad — clean excerpt
-			 * */
-			if ( ! $short_description_exist ) {
-
-				if ( ! get_option( 'wooms_short_description' ) ) {
-
-					$product->set_short_description( '' );
-				} else {
-
-					$product->set_description( '' );
-				}
-			}
-		}
-
-		return $product;
-	}
-
-	/**
-	 * To prevent Wooms XT (Extra) from saving field with a short description like product attribute
-	 *
-	 * @param  array $product_attributes — with instances of WC_Product_Attribute.
-	 * @param  int   $product_id — data from MoySklad.
-	 * @param  array $item — data from MoySklad.
-	 * @return array
-	 */
-	public static function remove_short_description_from_product_attributes( $product_attributes, $product_id, $item ) {
-
-		$field_name = get_option( 'wooms_excerpt_name' );
-
-		foreach ( $product_attributes as $slug => $attribute ) {
-
-			if ( $attribute->get_name() == $field_name ) {
-
-				unset( $product_attributes[ $slug ] );
-
-				break;
-			}
-		}
-
-		return $product_attributes;
 	}
 }
 
